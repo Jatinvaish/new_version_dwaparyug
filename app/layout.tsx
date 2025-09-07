224dc05003e309d4705b9f8ac92d6d80f4e5594a
amanjoshi254 <amanjoshi254@gmail.com>
2025-09-07 22:26:57 +0530

    feat: added canonical

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,22 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NextAuthProvider from "@/providers/providers";
 import ToasterProvider from "@/components/ui/sooner";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Dwaparyug NGO - Foundation",
-  description: "NGO Welfare Society dedicated to social causes and community development",
+  metadataBase: new URL("https://www.dwaparyug.org"), // 👈 your domain here
+  title: {
+    default: "Dwaparyug NGO - Foundation",
+    template: "%s | Dwaparyug Foundation",
+  },
+  description:
+    "NGO Welfare Society dedicated to social causes and community development",
   generator: "AJPR WORLD",
+  alternates: {
+    canonical: "/", // 👈 this makes https://www.dwaparyug.org the default canonical
+  },
 };
 
 export default function RootLayout({
