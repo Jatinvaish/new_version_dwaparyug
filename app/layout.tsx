ddbcc4d18d2e13a572e0688c417cd1f8d684ab64
amanjoshi254 <amanjoshi254@gmail.com>
2025-09-10 12:06:59 +0530

    feat: added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import NextAuthProvider from "@/providers/providers";
 import ToasterProvider from "@/components/ui/sooner";
 import { Analytics } from "@vercel/analytics/next";
+import Script from "next/script"; // ✅ import Next.js Script
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -46,6 +47,22 @@ export default function RootLayout({
           <ToasterProvider />
           {children}
           <Analytics />
+
+          {/* ✅ Google Analytics */}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-D9CYQ2RPGP"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-D9CYQ2RPGP', {
+                page_path: window.location.pathname,
+              });
+            `}
+          </Script>
         </NextAuthProvider>
       </body>
     </html>
