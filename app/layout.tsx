4fb934648c4af46e86db80c36597d94293835280
dwaparyugfoundation-cell <dwaparyugfoundation@gmail.com>
2025-10-12 14:16:15 +0000

    feat: done nothing

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import "./globals.css";
 import NextAuthProvider from "@/providers/providers";
 import ToasterProvider from "@/components/ui/sooner";
 import { Analytics } from "@vercel/analytics/next";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import Script from "next/script";
-import { GoogleTagManager } from "@next/third-parties/google";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import Script from "next/script"; // ✅ import Next.js Script
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -31,7 +31,7 @@ export const metadata: Metadata = {
     "NGO welfare society India",
     "transparent donation platform",
     "relief campaigns India",
-    "social development NGO",
+    "social development NGO"
   ],
   generator: "AJPR WORLD",
   alternates: {
@@ -46,37 +46,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        {/* ✅ Meta Pixel Code */}
-        <Script
-          id="meta-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1216856133825801');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1216856133825801&ev=PageView&noscript=1"
-          />
-        </noscript>
-        {/* ✅ End Meta Pixel Code */}
-      </head>
-
       <body className={inter.className}>
         <NextAuthProvider>
           <ToasterProvider />
