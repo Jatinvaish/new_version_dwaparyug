a15f71e67e3bd1e3fbedf3f3f0f1f259bb9d1761
Jatin Vaishnav <dwaparyugfoundation@gmail.com>
2025-11-09 15:17:32 +0530

    feat: update to function

--- app/(public)/contact-us/page.tsx
+++ app/(public)/contact-us/page.tsx
@@ -1,7 +1,7 @@
 import ContactComponent from '@/components/shared/contactus'
 import React from 'react'
 
-export const ContactPage = () => {
+export default function ContactPage() {
   return (
     <ContactComponent />
   )
